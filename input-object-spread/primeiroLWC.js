35d818bfe684e34abe81fe1be9c6b40d91dc20a3
AlissonJoaquim <128437734+AlissonJoaquim@users.noreply.github.com>
2023-04-28 08:36:13 -0300

    Update primeiroLWC.js

--- input-object-spread/primeiroLWC.js
+++ input-object-spread/primeiroLWC.js
@@ -1,6 +1,6 @@
 import { LightningElement, track } from 'lwc';
 
-export default class Lwc_iii_2 extends LightningElement {   
+export default class primeiroLWC extends LightningElement {   
 
    inputs = {
       firstname: '',
